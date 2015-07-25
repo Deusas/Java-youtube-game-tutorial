dffcca3f8b89ef1f7ebbd5f863481d1c0259640a
Jeffrey Spång <Deusas.1991@gmail.com>
2015-07-26 01:16:55 +0200

    Level
    Level stubs

--- src/jeffrey/rain/level/Level.java
+++ src/jeffrey/rain/level/Level.java
@@ -1,5 +1,40 @@
 package jeffrey.rain.level;
 
-public class Level {
+import jeffrey.rain.graphics.Screen;
 
+public class Level {
+	
+	protected int width, height;
+	protected int[] tiles;
+	
+	public Level(int width, int height) {
+		this.width = width;
+		this.height = height;
+		tiles = new int[width * height];
+		generateLevel();
+	}
+	
+	public Level(String path) {
+		loadLevel(path);
+	}
+	
+	protected void generateLevel(){
+		
+	}
+	
+	private void loadLevel(String path){
+		
+	}
+	
+	public void update() {
+		
+	}
+	
+	public void time() {
+		
+	}
+	
+	public void render(int xScroll, int yScroll, Screen screen) {
+		
+	}
 }
